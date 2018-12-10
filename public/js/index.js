b91ae76112c0e9b44e7924b94ad3409c5f86e6a1
Amel <amelsaifennasser@gmail.com>
2018-12-10 16:58:09 +0100

    add mmoment(createdAt)

--- public/js/index.js
+++ public/js/index.js
@@ -5,16 +5,17 @@ socket.on('connect', function(){
 });
 
 socket.on('newMessage', function(message){
-  console.log('nouveau message de serveur', message);
+  var formattedTime= moment(message.createdAt).format('h:mm a');
   var li= jQuery('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  li.text(`${formattedTime} ${message.from} :  ${message.text}`);
   jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(message){
+var formattedTime= moment(message.createdAt).format('h:mm a');
 var li= jQuery('<li></li>');
 var a= jQuery('<a target="_blank">My current location:</a>')
-li.text(`${message.from}:`);
+li.text(`${formattedTime} ${message.from} : `);
 a.attr('href', message.url);
 li.append(a);
 jQuery('#messages').append(li);
